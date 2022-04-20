8692a07afbdd50edae8352fe6e04608adf8b268d
Freddyramirez19 <48269797+Freddyramirez19@users.noreply.github.com>
2022-04-19 22:09:23 -0500

    Create quesiton 1.c

---
+++ quesiton 1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "common_threads.h"
+
+sem_t s; 
+
+void *child(void *arg) {
+  printf("child\n");
+  // use semaphore here
+
+  sem_wait(&s)
+  sleep(1);
+  return NULL;
+}
+
+int main(int argc, char *argv[]) {
+  pthread_t p;
+  printf("parent: begin\n");
+  // init semaphore here
+  sem_init(&s, 1, 1);
+
+  Pthread_create(&p, NULL, child, NULL);
+  // use semaphore here
+  sem_wait(&s);
+
+  printf("parent: end\n");
+  return 0;
+}
+
+/*
+gcc -o fork-join fork-join.c -pthread
+./fork-join
+*/
